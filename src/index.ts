8e0b5e71a93d8b41472aabb25e1779627547df4e
SaltyAom <aomkirby123@gmail.com>
2020-10-24 15:12:47 +0700

    :tada: feat - compression

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,112 @@
-import { IgetByUID, Iuniversity } from "./types"
-import u from "./universities.json";
+import universitiesData from '../universities.json'
 
-const getUniversities = () :Iuniversity => {
-    return {"data":u };
-}
-const getByUID = (uid: string) : IgetByUID => {
-        const found = u.filter(r => r.uid === uid);   
-        return {"data" : found[0]};
+import {
+    GetByUID,
+    Property,
+    Properties,
+    University,
+    Universities,
+    UniversitiesData
+} from './types'
+
+const notUniversity = [9, 10],
+    notEndWithUniversity = [
+        ...notUniversity,
+        49,
+        50,
+        51,
+        52,
+        53,
+        54,
+        55,
+        56,
+        57,
+        63,
+        64,
+        65,
+        69,
+        83,
+        86,
+        92,
+        105,
+        112,
+        116,
+        120
+    ],
+    properties: Properties = [
+        'th_name',
+        'en_name',
+        'short_name_th',
+        'short_name_en'
+    ]
+
+let universities: University[] = []
+
+const getUniversitiesData = () => {
+    if (universities.length) return universities
+
+    const parsedUniversities: Universities = universitiesData.map(
+        (university, _uid) => {
+            const uid = _uid + 1
+
+            return Object.assign(
+                { uid },
+                ...properties.map((property, index) => {
+                    let universityFieldValue: string | number
+
+                    const isUniversity = !notUniversity.includes(uid),
+                        isEndWithUniversity = !notEndWithUniversity.includes(
+                            uid
+                        )
+
+                    switch (property) {
+                        case 'th_name':
+                            universityFieldValue =
+                                (isUniversity ? 'มหาวิทยาลัย ' : 'สถาบัน ') +
+                                university[index]
+                            break
+
+                        case 'en_name':
+                            universityFieldValue =
+                                university[index] +
+                                (isEndWithUniversity ? ' University' : '')
+                            break
+
+                        default:
+                            universityFieldValue = university[index]
+                            break
+                    }
+
+                    return {
+                        [property]: universityFieldValue
+                    } as Pick<University, Property>
+                })
+            )
+        }
+    )
+
+    universities = parsedUniversities
+
+    return parsedUniversities
 }
 
-export {getUniversities, getByUID }
+const getUniversities = (): UniversitiesData => {
+    return { data: getUniversitiesData() }
+}
+
+/**
+ * @deprecated since 1.1.1. Use getUniversityByUID instead.
+ * 
+ * @param uid {number}
+ */
+const getByUID = (uid: number): GetByUID => getUniversityByUID(uid)
+
+const getUniversityByUID = (uid: number): GetByUID => {
+    const university =
+        getUniversitiesData().find((university) => university.uid === uid) ||
+        null
+
+    return { data: university }
+}
+
+export { getUniversities, getByUID, getUniversityByUID }
--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,22 @@
-export interface Iuniversity {
-    data: {
-        uid: string;
-        th_name: string;
-        en_name: string;
-        short_name_th: string;
-        short_name_en: string;
-    }[]
+export type ValueOf<T> = T[keyof T]
+
+export type Property = keyof Omit<University, 'uid'>
+export type Properties = Array<Property>
+
+export interface University {
+    uid: number
+    th_name: string
+    en_name: string
+    short_name_th: string
+    short_name_en: string
 }
 
-export interface IgetByUID {
-    data: {
-        uid: string;
-        th_name: string;
-        en_name: string;
-        short_name_th: string;
-        short_name_en: string;
-    } | null
+export type Universities = University[]
+
+export interface UniversitiesData {
+    data: Universities
+}
+
+export type GetByUID = {
+    data: University | null
 }
